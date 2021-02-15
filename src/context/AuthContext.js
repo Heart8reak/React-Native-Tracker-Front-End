1041219f23918b5a3d313974690634bf63c02525
Carlos Baez <c.baez1223@yahoo.com>
2021-02-14 22:17:11 -0500

    after signing up navigates to tracklist screen authentication is working!

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,24 +1,30 @@
+import { AsyncStorage } from 'react-native'
 import createDataContext from './createDataContext'
 import trackerApi from '../api/tracker'
+import { navigate } from '../navigationRef'
 
 const authReducer = (state, action) => {
     switch(action.type) {
         case 'add_error':
             return { ...state, errorMessage: action.payload }
+        case 'signup':
+            return { errorMessage: '', token: action.payload }
         default: 
         return state 
     }
 }
 
-const signup = (dispatch) => {
-    return async ({ email, password }) => {
-        try {
-            const response = await trackerApi.post('/signup', { email, password })
-            console.log(response.data)
-        } catch (err) {
-            dispatch({ type: 'add_error', payload: 'Something went wrong with signing up!' })
-            console.log(err.response.data)
-        }
+const signup = dispatch => async ({ email, password }) => {
+    try {
+        const response = await trackerApi.post('/signup', { email, password })
+        await AsyncStorage.setItem('token', response.data.token)
+        dispatch({ type: 'signup', payload: response.data.token })
+
+        // console.log(response.data)
+        navigate('TrackList')
+    } catch (err) {
+        dispatch({ type: 'add_error', payload: 'Something went wrong with signing up!' })
+        console.log(err.response.data)
     }
 }
 
@@ -39,5 +45,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
     authReducer,
     { signin , signout, signup },
-    { isSignedIn: false, errorMessage: ''}   
+    { token: null, errorMessage: ''}   
 )
